4a910f524e2e1356ca341bc78f623039ea385dd3
dummie kaphala <dummiekaphala7@gmail.com>
2022-07-27 10:37:52 +0200

    First Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>KP Lets get this shit Running</Text>
+      <Text>Wrote this Text</Text>
+      <Text>Another Change</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
