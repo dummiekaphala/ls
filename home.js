7aefbe29ce20000d2d61e3ecacf7d0bb32a93bdc
dummie kaphala <dummiekaphala7@gmail.com>
2022-07-27 12:50:07 +0200

    Add HOme

---
+++ home.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome To th Home Page</Text>
+      <Text>LifeStyle Is A proudly Malawian App to add vaslue to entertainment</Text>
+      <Text>Another Change</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
